92acc3b5053116d92175a1e1139b5c17299d432e
Fernanda-15 <fernandaqp15@gmail.com>
2022-04-06 22:20:09 -0600

    Agregando proyecto de Backend

---
+++ SGPDRAT/app/Models/Tarea.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tarea extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'proyecto_id',
+        'numero',
+        'descripcion',
+        'peso',
+        'avance'
+    ];
+
+    public $incrementing = true; //SE SETEA EL AUTOINCREMENTAL
+    protected $table = 'tarea'; //SE DEFINE EL NOMBRE DE LA TABLA
+
+
+    public function proyecto(){
+        return $this->belongsTo('App\Models\Proyecto','proyecto_id');
+    }
+}
