7a0c75c3571bba97ffb241d1052956e41aa2aabd
Allan <allanhansey07@gmail.com>
2022-05-16 13:30:49 -0600

    16mayo1330 backup

--- webSGPDRAT/src/app/components/proyecto/proyecto-info/proyecto-info.component.ts
+++ webSGPDRAT/src/app/components/proyecto/proyecto-info/proyecto-info.component.ts
@@ -4,6 +4,8 @@ import { TareaService } from 'src/app/services/tarea.service';
 import { Proyecto } from 'src/app/models/proyecto';
 import { Tarea } from 'src/app/models/tarea';
 import{Router,ActivatedRoute} from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
+import { User } from 'src/app/models/user';
 
 
 @Component({
@@ -11,21 +13,70 @@ import{Router,ActivatedRoute} from '@angular/router';
   templateUrl: './proyecto-info.component.html',
   styleUrls: ['./proyecto-info.component.css'],
   providers: [TareaService,
-            ProyectoService]
+            ProyectoService,
+          UserService]
 })
 export class ProyectoInfoComponent implements OnInit {
 
   public proyecto:Proyecto;
+  private user:User;
+  public userName:string;
   constructor(
     private _proyectoService:ProyectoService,
     private _tareaService:TareaService,
+    private _userService:UserService,
     private _route:ActivatedRoute, 
     private _router:Router,
   ) { 
     this.proyecto = new Proyecto(0,0,"","","","","","","",0);
+    this.user = new User(0,"","","","","","","",);
+    this.userName = "";
   }
 
   ngOnInit(): void {
+    this.getProyecto();
+  }
+
+  getProyecto():void{
+    this._route.params.subscribe(params=>{
+      let id=params['id'];
+      console.log(id);
+      this._proyectoService.getProyecto(id).subscribe(
+        response=>{
+          if(response.status=='success'){
+            this.proyecto=response.data;
+            console.log(this.proyecto);
+            this.getUserName();
+          }else{
+            console.log('AQUI');
+            //this._router.navigate(['']);
+          }
+        },
+        error=>{
+          console.log(error);
+         //this._router.navigate(['']); 
+        }
+      );
+    });
+  }
+
+  getUserName(){
+      this._userService.getUser(this.proyecto.user_id).subscribe(
+        response=>{
+          if(response.status=='success'){
+            this.user=response.data;
+            console.log(this.user);
+            this.userName = this.user.nombreUsuario;
+          }else{
+            console.log('AQUI');
+            //this._router.navigate(['']);
+          }
+        },
+        error=>{
+          console.log(error);
+         //this._router.navigate(['']); 
+        }
+      );
   }
 
 
