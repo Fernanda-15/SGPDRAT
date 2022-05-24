84fdcdd879bb8c30e1018839a43d39e2f8daa83b
Fernanda-15 <fernandaqp15@gmail.com>
2022-05-23 19:17:26 -0600

    Component tarea-avance

---
+++ webSGPDRAT/src/app/components/tarea/tareas-avance/tareas-avance.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tareas-avance',
+  templateUrl: './tareas-avance.component.html',
+  styleUrls: ['./tareas-avance.component.css']
+})
+export class TareasAvanceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
