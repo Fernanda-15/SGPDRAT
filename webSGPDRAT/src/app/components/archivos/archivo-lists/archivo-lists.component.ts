1b7635d194641d4348d83f447a33dc2a88962751
Fernanda-15 <fernandaqp15@gmail.com>
2022-05-31 01:24:16 -0600

    Componentes para listar fotos y archivos

---
+++ webSGPDRAT/src/app/components/archivos/archivo-lists/archivo-lists.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-archivo-lists',
+  templateUrl: './archivo-lists.component.html',
+  styleUrls: ['./archivo-lists.component.css']
+})
+export class ArchivoListsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
