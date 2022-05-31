e6b68cb5b9941fd8be15db2cfad69783473a3283
Fernanda-15 <fernandaqp15@gmail.com>
2022-05-31 10:05:17 -0600

    Arreglos archivos-list

--- webSGPDRAT/src/app/components/archivos/archivos-list/archivos-list.component.ts
+++ webSGPDRAT/src/app/components/archivos/archivos-list/archivos-list.component.ts
@@ -8,6 +8,7 @@ import { ArchivosService } from 'src/app/services/archivos.service';
 import { global } from 'src/app/services/global';
 import{Router,ActivatedRoute} from '@angular/router';
 import { PageEvent } from '@angular/material/paginator';
+import{timer} from 'rxjs';
 
 @Component({
   selector: 'app-archivos-list',
@@ -25,6 +26,8 @@ export class ArchivosListComponent implements OnInit {
   public desde:number = 0;
   public hasta:number = 3;
   public url:any;
+  public status:number=-1;
+
   constructor(
     private _route:ActivatedRoute,
     private _router:Router,
@@ -85,4 +88,70 @@ export class ArchivosListComponent implements OnInit {
       }
     );
   }
+
+  delete(id:number):void{
+    let counter=timer(5000);
+    this._fotosService.deleteFoto(id).subscribe(
+      response=>{
+        if(response.status=="success"){
+          this.status = -1;
+          this.loadFotos(this.inspeccion.id);
+          this.status=0;
+          counter.subscribe(n=>{
+            console.log(n);
+            this.status=-1;
+          });
+        }
+        else{
+          this.status=2;
+          counter.subscribe(n=>{
+            console.log(n);
+            this.status=-1;
+          });
+        }
+
+      },
+      error=>{
+        console.log(error);
+      }
+    );
+  }
+
+  delete2(id:number):void{
+    let counter=timer(5000);
+    this._archivoService.deletearchivos(id).subscribe(
+      response=>{
+        if(response.status=="success"){
+          this.status = -1;
+          this.loadDocumento(this.inspeccion.id);
+          this.status=0;
+          counter.subscribe(n=>{
+            console.log(n);
+            this.status=-1;
+          });
+        }
+        else{
+          this.status=2;
+          counter.subscribe(n=>{
+            console.log(n);
+            this.status=-1;
+          });
+        }
+
+      },
+      error=>{
+        console.log(error);
+      }
+    );
+  }
+
+
+  cambiarpagina(e:PageEvent){
+    console.log(e);
+    this.desde = e.pageIndex * e.pageSize;
+    this.hasta = this.desde + e.pageSize;
+
+  }
+
+  
 }
