3f2c2df7d2cf6db820de518a67c81f288c7ea8d4
Fernanda-15 <fernandaqp15@gmail.com>
2022-06-07 16:31:03 -0600

    Verificar que usuario no exista al registrar

--- webSGPDRAT/src/app/components/user/user-register/user-register.component.ts
+++ webSGPDRAT/src/app/components/user/user-register/user-register.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { User } from '../../../models/user';
 import { UserService } from '../../../services/user.service';
 import{Router,ActivatedRoute} from '@angular/router';
-
+import{timer} from 'rxjs';
 
 @Component({
   selector: 'app-user-register',
@@ -16,7 +16,7 @@ export class UserRegisterComponent implements OnInit {
   public user:User;
   public status:number;
   public reset=false;
-
+  public users:any[]=[];
   constructor(
     private _userService: UserService,
     private _router:Router,
@@ -30,29 +30,79 @@ export class UserRegisterComponent implements OnInit {
   }*/
 
   ngOnInit(): void {
-    console.log('COMPONENTE DE REGISTRO')
+    console.log('COMPONENTE DE REGISTRO');
+    this.getUsers();
   }
 
   onSubmit(form:any){
+  let counter=timer(5000);
+
     console.log(this.user);
-    this._userService.registro(this.user).subscribe(
-     response=>{
-      console.log(response);
-        if(response.status == "success"){
-          this.status = 0;
-          form.reset();
-          this._router.navigate(['/user-list']);
+    if(this.existeUs()){ //VERIDICAR SI EXISTE TAREA
+      this._userService.registro(this.user).subscribe(
+        response=>{
+         console.log(response);
+           if(response.status == "success"){
+             form.reset();
+             this._router.navigate(['/user-list']);
+   
+            }else{
+                     this.status=1;
+                     counter.subscribe(n=>{
+                      console.log(n);
+                      this.status=-1;
+                    });
+                  }
+          },
+         error=>{
+           this.status = 1;
+           counter.subscribe(n=>{
+            console.log(n);
+            this.status=-1;
+          });
+          console.log(<any>error);
+   
+         }
+      );
+    }else{
+      this.status=2;
+      counter.subscribe(n=>{
+        console.log(n);
+        this.status=-1;
+      });
+    }
+   
+  }
 
-         }else{
-                  this.status=1;
-               }
-       },
-      error=>{
-        this.status = 1;
-       console.log(<any>error);
+  getUsers(){
+    this._userService.getUsers().subscribe(
+      response=>{
+        console.log(response);
+          if(response.status == "success"){
+            this.users=response.data;
+          }
+         },
+        error=>{
+         this.status = 1;
+         console.log(<any>error);
+  
+        }
+    );
+  }
+  existeUs():any{
+    console.log(this.users); 
 
+    for(let i in this.users){
+      if(this.users[i].nombreUsuario == this.user.nombreUsuario){
+        return false;
+      }else if(this.users[i].cedula == this.user.cedula){
+        return false;
+      }else if(this.users[i].correo == this.user.correo){
+        return false;
       }
-   );
+    }
+   
+    return true;
   }
 
 
